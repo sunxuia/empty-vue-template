552c4a593c59003e5a61a58c3e1d4d47c4015e02
sunxu <ntsunxu@163.com>
2018-12-16 21:00:31 +0800

    add camelCase option

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,12 @@ const isProduction = process.env.NODE_ENV === 'production'
 module.exports = {
   baseUrl: './',
   css: {
-    sourceMap: !isProduction
+    sourceMap: !isProduction,
+    loaderOptions: {
+      css: {
+        camelCase: 'dashes'
+      }
+    }
   },
   configureWebpack: {
     plugins: [
