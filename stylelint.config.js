a12923182cf2c4cd74a6e0193120590fc8447fd9
sunxu <ntsunxu@163.com>
2018-12-16 19:26:41 +0800

    add stylelint scss rule

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  'extends': 'stylelint-config-recommended',
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss'
+  ],
+  plugins: [
+    'stylelint-scss'
+  ],
   rules: {
     'indentation': [2, {
       baseIndentLevel: 0
